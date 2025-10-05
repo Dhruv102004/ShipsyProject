d9f8a5a60eade9b683ff81def03cf47da916a1d4
Dhruv102004 <dhruvagrawals@gmail.com>
2025-10-05 11:52:52 +0530

    push

--- prettier.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('prettier').Config} */
-module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindAttributes: ["theme"],
-  tailwindFunctions: ["twMerge", "createTheme"],
-};
